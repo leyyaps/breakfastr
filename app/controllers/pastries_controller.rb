5c2f44aaaf0a11bd01d2aaa067fcdff886a2d3d4
Andrew Smith1 <AndrewSmith1@MacBook-Pro.local>
2015-03-05 14:51:35 +0000

    deleted incorrect views page

--- app/controllers/pastries_controller.rb
+++ app/controllers/pastries_controller.rb
@@ -8,9 +8,48 @@ class PastriesController < ApplicationController
   	
   end
 
-  def view
+  def new
+    @pastry = Pastry.new
+  end
+
+  def create
+    @pastry = Pastry.new(pastry_params)
+    if @pastry.save
+        flash[:success] = "Pastry added!"
+        redirect_to root_path
+      else
+        flash[:error] = "oops there is something wrong with your recipe there"
+        render :new
+      end
   end
 
   def edit
+    @pastry = Pastry.find(params[:id])
   end
+
+  def update
+    @pastry = Pastry.find(params[:id])
+    if @pastry.update(pastry_params)
+      flash[:success] = "Pastry has been successfully updated"
+      redirect_to pastry_path(@pastry)
+    else
+      flash[:error] = "Oops, you fucked up"
+      render = :edit
+    end
+  end
+
+  def destroy
+    pastry = Pastry.find(params[:id])
+    pastry.destroy
+    flash[:success] = "You destroyed a perfectly good pastry"
+    redirect_to root_path
+  end
+
+  private
+  def pastry_params
+      params.require(:pastry).permit(:name, :price_in_pence, :description)
+  end
+
 end
+
+
