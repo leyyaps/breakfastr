0050ef3c502d89966da61b8e04d8b819149acb0c
Andrew Smith1 <AndrewSmith1@MacBook-Pro.local>
2015-03-05 12:20:55 +0000

    loop over pastries hash

--- app/controllers/pastries_controller.rb
+++ app/controllers/pastries_controller.rb
@@ -1,5 +1,15 @@
 class PastriesController < ApplicationController
   def index
+  	@pastries = [{
+  		name: "Croissant",
+  		price_in_pence: 350,
+  		description: "A delicious rich, crescent-shaped roll of leavened dough or puff pastry"
+  		},
+  		{
+  		name: "Pain au Chocolat",
+  		price_in_pence: 380,
+  		description: "Fresh from the oven. It's golden and just slight crispy on the outside, with melted chocolate swirled throughout"
+  			}]
   end
 
   def show
